7f0be3b084852e2776ac59522d9ae6cd2dc57463
Donizete <juniorba94@outlook.com>
2022-05-01 18:55:13 -0300

    Operadores em Java

--- triagem-java/cursojavabasico/aula13/OperadoresAritmeticos.java
+++ triagem-java/cursojavabasico/aula13/OperadoresAritmeticos.java
@@ -25,5 +25,27 @@ public class OperadoresAritmeticos {
         String segundoNome = " uma String concatenada.";
         String terceiroNome = primeiroNome + segundoNome;
         System.out.println(terceiroNome);
+
+        resultado = resultado +1;
+        System.out.println(resultado);
+
+        resultado++;
+        System.out.println(resultado++);
+        //mesma coisa que
+        //System.out.println(resultado)
+        //resultado = resultado + 1;
+        //resultado += 1;
+
+        System.out.println(++resultado);
+        //mesma coisa que
+        //resultado += 1;
+        //System.out.println(resultado);
+
+        resultado--;
+        System.out.println(resultado);
+
+        System.out.println(resultado--);
+        System.out.println(--resultado);
+
     }
 }
