643083582c0d78e84f05702eefa65efc752acd16
Donizete <juniorba94@outlook.com>
2022-09-10 16:42:44 -0300

    Desafios

--- front-end/fundamentos/inicio/desafio_area.js
+++ front-end/fundamentos/inicio/desafio_area.js
@@ -1,8 +1,9 @@
-const PI = 3.141592
+const PI = 3.1411592;
 const raio = 10;
-let areaCirc = PI * raio * raio;
 
-console.log("O valor da área é " + areaCirc + " m2.")
+let area = PI * raio * raio;
+console.log("O valor do raio é "  + raio + " m.");
+console.log("O valor da area é " + area + " m2.");
 
-areaCirc = Math.PI * raio * raio;
-console.log("O valor da área é " + areaCirc + "m2.")
+area = Math.PI * raio * raio;
+console.log("O valor da área é " + area + " m2.")
