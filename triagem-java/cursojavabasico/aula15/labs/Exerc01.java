3864442e18c11a5525de559e74939b85bf5ce1fc
Donizete Batista Junior <donizetej@ciandt.com>
2022-09-13 17:03:35 -0300

    Exerc parte 1

--- triagem-java/cursojavabasico/aula15/labs/Exerc01.java
+++ triagem-java/cursojavabasico/aula15/labs/Exerc01.java
@@ -6,17 +6,15 @@ public class Exerc01 {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-        System.out.println("Entre com o primeiro número");
+        System.out.println("Digite o primeiro número");
         int num1 = scan.nextInt();
-
-        System.out.println("Entre com o segundo número");
+        System.out.println("Digite o segundo número");
         int num2 = scan.nextInt();
 
         if(num1 > num2){
-            System.out.println("O num1 é maior"+ num1);
-        } else {
-            System.out.println("O num2 é maior:" + num2);
+            System.out.println("o num1 é maior: " +num1);
+        }else{
+            System.out.println("o num2 é maior: " + num2);
         }
     }
 }
