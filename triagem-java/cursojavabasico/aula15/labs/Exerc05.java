3ab05c7918db0f14f0e2d2fe86f3a2c77376a524
Donizete <juniorba94@outlook.com>
2022-09-27 17:24:01 -0300

    Exercicios parte 2

--- triagem-java/cursojavabasico/aula15/labs/Exerc05.java
+++ triagem-java/cursojavabasico/aula15/labs/Exerc05.java
@@ -1,4 +1,25 @@
 package aula15.labs;
 
+import java.util.Scanner;
+
 public class Exerc05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira a primeira nota: ");
+        double nota1 = scan.nextDouble();
+        System.out.println("Insira a segunda nota: ");
+        double nota2 = scan.nextDouble();
+
+        double media = (nota1 + nota2)/ 2;
+        if(media == 10) {
+            System.out.println("Aprovado com Distinção");
+        }else if(media >= 7){
+            System.out.println("Aprovado");
+        }else{
+            System.out.println("Reprovado");
+        }
+
+
+    }
 }
--- triagem-java/cursojavabasico/aula15/labs/Exerc07.java
+++ triagem-java/cursojavabasico/aula15/labs/Exerc07.java
@@ -1,4 +1,34 @@
 package aula15.labs;
 
+import java.util.Scanner;
+
 public class Exerc07 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com primeiro número:");
+        int num1 = scan.nextInt();
+
+        System.out.println("Entre com segundo número:");
+        int num2 = scan.nextInt();
+
+        System.out.println("Entre com terceiro número:");
+        int num3 = scan.nextInt();
+
+        if(num1 >= num2 && num1 >= num3){
+            System.out.println("num1 é o maior: " + num1);
+        } else if(num2 >= num1 && num2 >= num3){
+            System.out.println("num2 é o maior: " + num2);
+        } else if (num3 >= num1 && num3 >= num2){
+            System.out.println("num3 é o maior: " + num3);
+        }
+
+        if(num1 <= num2 && num1 <= num3){
+            System.out.println("num1 é o menor: " + num1);
+        } else if(num2 <= num1 && num2 <= num3){
+            System.out.println("num2 é o menor: " + num2);
+        } else if (num3 <= num1 && num3 <= num2){
+            System.out.println("num3 é o menor: " + num3);
+        }
+    }
 }
