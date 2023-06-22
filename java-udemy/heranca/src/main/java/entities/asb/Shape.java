f0064b127087c4ef868a8b3621c8865f1a514876
Donizete <juniorba94@outlook.com>
2023-06-21 22:55:05 -0300

    Metodos Abstratos

---
+++ java-udemy/heranca/src/main/java/entities/asb/Shape.java
@@ -0,0 +1,25 @@
+package entities.asb;
+
+import entities.asb.enums.Color;
+
+public abstract class Shape {
+
+    private Color color;
+
+    public Shape() {
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
+}
---
+++ java-udemy/heranca/src/main/java/entities/asb/enums/Color.java
@@ -0,0 +1,7 @@
+package entities.asb.enums;
+
+public enum Color {
+    BLACK,
+    BLUE,
+    RED;
+}
