d65178c450e2ca6e99c004836c019621675773ff
Donizete Batista Junior <donizetej@ciandt.com>
2023-10-03 14:17:34 -0300

    Enumeradores Construtores e metodos

---
+++ triagem-java/javaintermediario/src/main/java/aula54/TesteEnum.java
@@ -0,0 +1,15 @@
+package aula54;
+
+public class TesteEnum {
+
+  public static void main(String[] args) {
+
+    DiaSemana dia = DiaSemana.DOMINGO;
+
+    System.out.println(dia + " - " + dia.getValor());
+
+    Data data = new Data(1, 4, 2016, DiaSemana.SEXTA);
+
+
+  }
+}
