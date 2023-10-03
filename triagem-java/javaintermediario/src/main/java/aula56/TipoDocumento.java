c7adfce9a4035890a295074d383af5b647d579ac
Donizete Batista Junior <donizetej@ciandt.com>
2023-10-03 17:04:52 -0300

    metodos abstratos

---
+++ triagem-java/javaintermediario/src/main/java/aula56/TipoDocumento.java
@@ -0,0 +1,18 @@
+package aula56;
+
+public enum TipoDocumento {
+
+  CPF {
+    @Override
+    public String gerarNumeroTeste() {
+      return GerarCpfCnpj.cpf();
+    }
+  }, CNPJ {
+    @Override
+    public String gerarNumeroTeste() {
+      return GerarCpfCnpj.cnpj();
+    }
+  };
+
+  public abstract String gerarNumeroTeste();
+}
