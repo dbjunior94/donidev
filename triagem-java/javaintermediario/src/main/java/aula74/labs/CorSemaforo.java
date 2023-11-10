96d833947625c4788efe4d5f8ffe36a00abc97b1
Donizete Batista Junior <donizetej@ciandt.com>
2023-11-10 16:28:10 -0300

    Threads

---
+++ triagem-java/javaintermediario/src/main/java/aula74/labs/CorSemaforo.java
@@ -0,0 +1,15 @@
+package aula74.labs;
+
+public enum CorSemaforo {
+   VERDE(1000), AMARELO(300), VERMELHO(2000);
+
+   private int tempoEspera;
+
+   CorSemaforo(int tempoEspera){
+      this.tempoEspera = tempoEspera;
+   }
+
+   public int getTempoEspera() {
+      return tempoEspera;
+   }
+}
